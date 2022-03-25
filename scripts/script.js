64895f7479555f78b00dc981ff69d2a7278989ad
CoreosG <coreosgg@gmail.com>
2022-03-25 17:07:27 -0300

    testando novo tipo de interface

--- scripts/script.js
+++ scripts/script.js
@@ -4,40 +4,62 @@
 //banner change image todo
 
 
-//navbar behaviour
+//elements on screen behaviour on scroll
 let prevScrollpos = window.pageYOffset;
+let elements = {
+    mainSection: document.getElementById("mainSection"),
+    form: document.getElementById("form"),
+    navbar: document.getElementById("navbar"),
+    btnEntrar: document.getElementById("btnEntrar")
+};
+
 window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-    let mainSectionY = document.getElementById("mainSection").getBoundingClientRect().bottom;
-    let formY = document.getElementById("form").getBoundingClientRect().bottom;
+    let currentScrollPos = window.scrollY;
+    let height = screen.height;
+    let mainSectionY = elements.mainSection.getBoundingClientRect().bottom;
+    // let formY = elements.form.getBoundingClientRect().top;
+    console.log(mainSectionY )
     if (mainSectionY < 0) {
         toggleMenuStyles(true);
         if (prevScrollpos > currentScrollPos) {
-            navbar.style.top = "-100px";
+            elements.navbar.style.top = "-100px";
             } else {
-            navbar.style.top = "0px";
+            elements.navbar.style.top = "0px";
             }
             prevScrollpos = currentScrollPos;
     } else {
         toggleMenuStyles(false);
     }
+    // console.log(formY, mainSectionY);
+    // if (formY <= 0){
+    //     togglePositionForm(true);
+    // }else if (mainSectionY >= height * 0.6){
+    //     togglePositionForm(false)
+    // }
 }
+
+//pos toggler for form
+// function togglePositionForm(toggle){
+//     if (toggle) {
+//         elements.form.style.top = "12%";
+//         elements.form.style.position = "fixed";
+//     } else {
+//         elements.form.style = "";
+//     }
+// }
 
 //style toggler for navbar
 function toggleMenuStyles(toggle){
-    let btnEntrar = document.getElementById("btnEntrar");
-    let navbar = document.getElementById("navbar");
     let aTagsNavBar = document.querySelectorAll(".link");
     if (toggle) {
-        navbar.style.position = "fixed";
-        navbar.style.marginTop = 0;
-        navbar.style.background = "white";
-        navbar.style.boxShadow = "0px 10px var(--orange)";
-        btnEntrar.style.display = "none";
+        elements.navbar.style.position = "fixed";
+        elements.navbar.style.backgroundColor = "white";
+        elements.navbar.style.boxShadow = "0px 10px var(--orange)";
+        // elements.btnEntrar.style.display = "none";
         aTagsNavBar.forEach(link => { link.style.color = "var(--black)"; })
     } else {
-        navbar.style = "";
-        btnEntrar.style = "";
+        elements.navbar.style = "";
+        // elements.btnEntrar.style = "";
         aTagsNavBar.forEach(link => { link.style = ""; })
     }
     
