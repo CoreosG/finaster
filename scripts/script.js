638e29936d602079a71ddab813c2fb2152bebd28
CoreosG <coreosgg@gmail.com>
2022-03-28 17:24:40 -0300

    resolvi mta treta nem sei e tem q resolve mais ainda

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
 
-
-
 //banner change image todo
 
 
@@ -10,33 +8,37 @@ let elements = {
     mainSection: document.getElementById("mainSection"),
     form: document.getElementById("form"),
     navbar: document.getElementById("navbar"),
-    btnEntrar: document.getElementById("btnEntrar")
+    btnEntrar: document.getElementById("btnEntrar"),
+    dropDownListWrapper: document.getElementById("dropDownListWrapper"),
+    doubleArrowImg: document.getElementById("doubleArrowImg"),
+    showSubMenu: document.getElementById("showSubMenu")
 };
+let globalVars = {};
 
 window.onscroll = function() {
     let currentScrollPos = window.scrollY;
     let height = screen.height;
     let mainSectionY = elements.mainSection.getBoundingClientRect().bottom;
-    // let formY = elements.form.getBoundingClientRect().top;
-    console.log(mainSectionY )
+    console.log(mainSectionY, prevScrollpos, currentScrollPos);
     if (mainSectionY < 0) {
-        toggleMenuStyles(true);
-        if (prevScrollpos > currentScrollPos) {
-            elements.navbar.style.top = "-100px";
+        if(!globalVars.navbarStateToggled) {
+            toggleMenuStyles(true);
+            globalVars.navbarStateToggled = true;
+        }
+
+        if (prevScrollpos >= currentScrollPos) {
+            elements.navbar.style.top = "-500px";
             } else {
             elements.navbar.style.top = "0px";
             }
             prevScrollpos = currentScrollPos;
     } else {
+        
         toggleMenuStyles(false);
     }
-    // console.log(formY, mainSectionY);
-    // if (formY <= 0){
-    //     togglePositionForm(true);
-    // }else if (mainSectionY >= height * 0.6){
-    //     togglePositionForm(false)
-    // }
+
 }
+
 
 //pos toggler for form
 // function togglePositionForm(toggle){
@@ -54,13 +56,26 @@ function toggleMenuStyles(toggle){
     if (toggle) {
         elements.navbar.style.position = "fixed";
         elements.navbar.style.backgroundColor = "white";
-        elements.navbar.style.boxShadow = "0px 10px var(--orange)";
-        // elements.btnEntrar.style.display = "none";
+        // elements.navbar.style.boxShadow = "0px 10px var(--orange)";
+        globalVars.dropDownList = false;
         aTagsNavBar.forEach(link => { link.style.color = "var(--black)"; })
     } else {
         elements.navbar.style = "";
-        // elements.btnEntrar.style = "";
         aTagsNavBar.forEach(link => { link.style = ""; })
     }
     
 }
+
+//drop down menu
+
+function showSubMenu() {
+    let show = elements.showSubMenu.checked;
+    if (show) {
+        elements.dropDownListWrapper.style.display = "block";
+        elements.doubleArrowImg.src = "./images/svg/setaCima.svg"
+    } else {
+        elements.dropDownListWrapper.style.display = "none";
+        elements.doubleArrowImg.src = "./images/svg/setaBaixo.svg";
+    }
+}
+// showSubMenu();
